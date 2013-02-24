aa1a3bd032dd7784cb4599683ac789cc54ff5196
Andrew Crites <explosion-pills@aysites.com>
2013-02-23 21:48:36 -0500

    Updating class names and adding support for ajax loading

--- jquery.paginator.js
+++ jquery.paginator.js
@@ -15,33 +15,40 @@
          //bottom navigation container element
          bottomNav: $("<div>").addClass(pluginName + '-container'),
          //template for page number button element
-         pageNumNav: $("<span>").addClass(pluginName + '-page-number'),
+         pageNumNav: $("<span>").addClass(pluginName + '-page-number ' + pluginName + '-nav'),
          //templates for first/last button elements
          showFirstLast: {
-            first: $("<span>").addClass(pluginName + '-first-page').text('First'),
-            last: $("<span>").addClass(pluginName + '-last-page').text('Last')
+            first: $("<span>").addClass(pluginName + '-first-page ' + pluginName + '-nav').text('First'),
+            last: $("<span>").addClass(pluginName + '-last-page ' + pluginName + '-nav').text('Last')
+         },
+         showPrevNext: {
+            prev: $("<span>").addClass(pluginName + '-prev-page ' + pluginName + '-nav').text('Prev'),
+            next: $("<span>").addClass(pluginName + '-next-page ' + pluginName + '-nav').text('Next')
          },
          //templates for prev/next button elements
          //These move the displayed page numbers by the provided number of steps
-         showPrevNext: {
-            prev: $("<span>").addClass(pluginName + '-prev-page').text('<<'),
-            next: $("<span>").addClass(pluginName + '-next-page').text('>>'),
+         showChunkPrevNext: {
+            prev: $("<span>").addClass(pluginName + '-prev-chunk ' + pluginName + '-nav').text('<<'),
+            next: $("<span>").addClass(pluginName + '-next-chunk ' + pluginName + '-nav').text('>>'),
             step: 5
          },
          //Maximum number of page numbers to display at a time
-         maxNavDisplay: 5,
+         maxNavDisplay: 20,
          //Last initial page number to display.  If false,
          //all page numbers are displayed simultaneously and
          //next/prev are not used
          navDisplay: 1,
          //Initial page number
-         changePage: 1
+         changePage: 1,
+         //ajax handling
+         ajax: false
       }
    ;
 
-   Plugin = function ($pages, userOptions) {
+   Plugin = function (pages, userOptions) {
+      this.element = pages;
       //element that the plugin is bound to
-      this.$pages = $pages;
+      this.$pages = $(pages);
       this.options = $.extend({}, defaultOptions, userOptions);
       var options = this.options;
       //falsy options do not get triggered
@@ -56,6 +63,7 @@
 
       this.pages = [];
       this.pageIndex = 0;
+      this.ajax = this.options.hasOwnProperty('ajax');
       /**#@+
        * Create empty selectors that will contain elements created
        * by the plugin for easy access/cleanup
@@ -68,6 +76,8 @@
       this.$last = $([]);
       this.$prev = $([]);
       this.$next = $([]);
+      this.$prevChunk = $([]);
+      this.$nextChunk = $([]);
       /**#@-*/
    };
 
@@ -75,24 +85,40 @@
       //Initialize the plugin, split up page elements and create navigation
       init: function () {
          var self = this;
-         //Loop over each delimiter found as a descendants of the container element
-         //Gather all elements between each delimiter (as well as the delimeter's
-         //children) and create pages
-         this.$pages.find(self.options.delimiter).each(function (index) {
-            if (!((index + self.options.perPage) % self.options.perPage)) {
-               self.pageIndex++;
-            }
-            if (!self.pages[self.pageIndex]) {
-               self.pages[self.pageIndex] = $([]);
-            }
-            self.pages[self.pageIndex] = self.pages[self.pageIndex].add($(this).nextUntil(self.options.delimiter).andSelf());
-         });
 
-         //skip pagination setup if there is only one page
-         if (typeof self.pages[2] === 'undefined') {
-            return;
+         //Set up pages for ajax handling; do not loop over the delimiter as
+         //page generation is dynamic
+         if (self.ajax) {
+            if (self.options.ajax.pages > 1) {
+               self.pages = new Array(self.options.ajax.pages + 1);
+               if (self.$pages.find(self.options.delimiter).length) {
+                  self.pages[1] = self.$pages.find(self.options.delimiter);
+               }
+            }
+            else {
+               return;
+            }
          }
-         self.pageIndex = this.options.changePage;
+         else {
+            //Loop over each delimiter found as a descendants of the container element
+            //Gather all elements between each delimiter (as well as the delimeter's
+            //children) and create pages
+            this.$pages.find(self.options.delimiter).each(function (index) {
+               if (!((index + self.options.perPage) % self.options.perPage)) {
+                  self.pageIndex++;
+               }
+               if (!self.pages[self.pageIndex]) {
+                  self.pages[self.pageIndex] = $([]);
+               }
+               self.pages[self.pageIndex] = self.pages[self.pageIndex].add($(this).nextUntil(self.options.delimiter).andSelf());
+            });
+
+            //skip pagination setup if there is only one page
+            if (typeof self.pages[2] === 'undefined') {
+               return;
+            }
+         }
+         self.pageIndex = +this.options.changePage;
 
          //Append navigation elements to the DOM
          this.$pages.before(this.options.topNav);
@@ -101,7 +127,7 @@
          this.$bottomNav = this.$bottomNav.add(this.options.bottomNav);
 
          //Initial navigation setup; this needs to be done in a specific order
-         $.each(['pageNumNav', 'showPrevNext', 'showFirstLast', 'navDisplay', 'changePage'], function () {
+         $.each(['pageNumNav', 'showPrevNext', 'showChunkPrevNext', 'showFirstLast', 'navDisplay', 'changePage'], function () {
             if (typeof self['trigger_' + this] === 'function' && self.options[this]) {
                self['trigger_' + this](self.options[this]);
             }
@@ -138,8 +164,34 @@
          }
       },
 
-      //Create elements for changing the displayed page numbers
       trigger_showPrevNext: function (elements) {
+         var self = this;
+         if (!self.options.navDisplay) {
+            return;
+         }
+
+         elements.prev.on('click', function () {
+            self.trigger_changePage(self.pageIndex - 1);
+         });
+         elements.next.on('click', function () {
+            self.trigger_changePage(self.pageIndex + 1);
+         });
+
+         var $prevTop = elements.prev.clone(true);
+         var $prevBot = elements.prev.clone(true);
+         var $nextTop = elements.next.clone(true);
+         var $nextBot = elements.next.clone(true);
+         self.$prev = self.$prev.add($prevTop).add($prevBot);
+         self.$next = self.$next.add($nextTop).add($nextBot);
+
+         self.$pageNumNavTop.first().before($prevTop);
+         self.$pageNumNavTop.last().after($nextTop);
+         self.$pageNumNavBottom.first().before($prevBot);
+         self.$pageNumNavBottom.last().after($nextBot);
+      },
+
+      //Create elements for changing the displayed page numbers
+      trigger_showChunkPrevNext: function (elements) {
          var self = this;
          if (!self.options.navDisplay) {
             return;
@@ -156,8 +208,8 @@
          var $prevBot = elements.prev.clone(true);
          var $nextTop = elements.next.clone(true);
          var $nextBot = elements.next.clone(true);
-         self.$prev = self.$prev.add($prevTop).add($prevBot);
-         self.$next = self.$next.add($nextTop).add($nextBot);
+         self.$prevChunk = self.$prevChunk.add($prevTop).add($prevBot);
+         self.$nextChunk = self.$nextChunk.add($nextTop).add($nextBot);
 
          //Prev/Next are only needed if the number of pages exceeds the
          //maximum number of page numbers that can be displayed
@@ -226,26 +278,41 @@
 
          //Change display of validity of prev/next
          if (this.$pageNumNavTop.first().is(':visible')) {
-            this.$prev.addClass(this._name + '-disabled');
+            this.$prevChunk.addClass(pluginName + '-disabled');
          }
          else {
-            this.$prev.removeClass(this._name + '-disabled');
+            this.$prevChunk.removeClass(pluginName + '-disabled');
          }
 
          if (this.$pageNumNavTop.last().is(':visible')) {
-            this.$next.addClass(this._name + '-disabled');
+            this.$nextChunk.addClass(pluginName + '-disabled');
          }
          else {
-            this.$next.removeClass(this._name + '-disabled');
+            this.$nextChunk.removeClass(pluginName + '-disabled');
          }
       },
 
       //Handle changing page to the provided number
       trigger_changePage: function (pageNum) {
-         if (typeof this.pages[pageNum] === 'undefined') {
-            return;
+         var self = this;
+         while (typeof this.pages[pageNum] === 'undefined') {
+            if (this.ajax && pageNum > 0 && pageNum <= this.options.ajax.pages) {
+               //Load html and store in pages array for faster future use
+               $.get(encodeURI(self.options.ajax.target), self.options.ajax.param + '=' + pageNum, function (html) {
+                  html = $(html).find(self.options.delimiter).hide();
+                  self.pages[pageNum] = html;
+                  self.$pages.append(html);
+                  self.trigger_changePage(pageNum);
+                  self.options.ajax.callback(html);
+               });
+               return;
+            }
+            pageNum--;
+            if (pageNum <= 0) {
+               return;
+            }
          }
-         this.pageIndex = pageNum;
+         this.pageIndex = +pageNum;
 
          //the hide-all then show some trick will not work because that
          //can screw up the page scrolling
@@ -257,24 +324,28 @@
          });
 
          //Update selected page number
-         this.$pageNumNavTop.removeClass(this._name + '-selected');
-         this.$pageNumNavTop.eq(pageNum - 1).addClass(this._name + '-selected');
-         this.$pageNumNavBottom.removeClass(this._name + '-selected');
-         this.$pageNumNavBottom.eq(pageNum - 1).addClass(this._name + '-selected');
+         this.$pageNumNavTop.removeClass(pluginName + '-selected');
+         this.$pageNumNavTop.eq(pageNum - 1).addClass(pluginName + '-selected');
+         this.$pageNumNavBottom.removeClass(pluginName + '-selected');
+         this.$pageNumNavBottom.eq(pageNum - 1).addClass(pluginName + '-selected');
 
          //Update first/last selected
          if (pageNum == 1) {
-            this.$first.addClass(this._name + '-disabled');
+            this.$first.addClass(pluginName + '-disabled');
+            this.$prev.addClass(pluginName + '-disabled');
          }
          else {
-            this.$first.removeClass(this._name + '-disabled');
+            this.$first.removeClass(pluginName + '-disabled');
+            this.$prev.removeClass(pluginName + '-disabled');
          }
 
          if (pageNum == this.pages.length - 1) {
-            this.$last.addClass(this._name + '-disabled');
+            this.$last.addClass(pluginName + '-disabled');
+            this.$next.addClass(pluginName + '-disabled');
          }
          else {
-            this.$last.removeClass(this._name + '-disabled');
+            this.$last.removeClass(pluginName + '-disabled');
+            this.$next.removeClass(pluginName + '-disabled');
          }
 
          //Current page number should be made visible on page change
@@ -282,11 +353,10 @@
 
          //Trigger an event that allows for additional handling of
          //page change external to the plugin
-         this.$pages.trigger(this._name + '.pageChange');
+         this.$pages.trigger(pluginName + '.pageChange');
       },
 
-      trigger_destroy: function () {
-         $.each(this.pages, function () { $(this).show(); });
+      trigger_destroy: function (pageCount) {
          //Remove all elements created by the plugin and appended to the DOM
          this.$topNav.remove();
          this.$bottomNav.remove();
@@ -296,14 +366,31 @@
          this.$last.remove();
          this.$prev.remove();
          this.$next.remove();
-         $.removeData(this.$pages, this._name)
+         this.$prevChunk.remove();
+         this.$nextChunk.remove();
+         if (this.ajax) {
+            this.pages = new Array(pageCount);
+            this.options.ajax.pages = pageCount;
+            this.$pages.find(this.options.delimiter).remove();
+         }
+         else {
+            $.each(this.pages, function () {
+               $(this).show();
+            });
+         }
+         $.removeData(this.element, pluginName)
+      },
+
+      trigger_recalculate: function (pageCount) {
+         this.trigger_destroy(pageCount);
+         this.$pages[pluginName]($.extend({}, this.options, {changePage: this.pageIndex}));
       }
    };
 
    $.fn[pluginName] = function (options) {
       return this.each(function () {
          if (!$.data(this, pluginName)) {
-            var paginator = new Plugin($(this), options);
+            var paginator = new Plugin(this, options);
             paginator.init();
             $.data(this, pluginName, paginator);
          }
